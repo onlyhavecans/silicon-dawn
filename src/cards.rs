ae39b947bd823abaf0c3a2fffd9a8b01aff118d4
Amelia Aronsohn <squirrel@wearing.black>
2021-12-10 14:19:27 -0800

    Use Options in deck and card retrieval
    When I was grocery shopping I had a conversation in my head with an imaginary person about how and why I do the get_cards, and I realized I was not using Options for exactly what they are designed for.

--- src/cards.rs
+++ src/cards.rs
@@ -1,7 +1,7 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::path::Path;
-use std::{fs, io};
+use std::{fs};
 use std::ffi::OsStr;
 
 pub type CardDeck = Vec<String>;
@@ -11,31 +11,34 @@ pub struct Card {
     pub text: String,
 }
 
-pub fn get_cards(directory: &str) -> Result<CardDeck, io::Error> {
+pub fn get_cards(directory: &str) -> Option<CardDeck> {
     let dir = Path::new(directory);
-    let files = fs::read_dir(dir)?;
+    let files = fs::read_dir(dir).ok()?;
     let extension: &OsStr = OsStr::new("jpg");
 
-    println!("Caching card list!");
     let cards: CardDeck = files
         .filter(|x| x.is_ok())
         .map(|x| x.unwrap())
         .filter(|x| x.path().extension() == Some(extension) )
         .map(|x| x.file_name().into_string().unwrap())
         .collect();
-    Ok(cards)
+
+    match cards.is_empty() {
+        true => None,
+        false => Some(cards),
+    }
 }
 
-pub fn pick_a_card(cards: &CardDeck) -> Result<Card, String> {
+pub fn pick_a_card(cards: &CardDeck) -> Option<Card> {
     let mut rng = thread_rng();
     let pick = cards.choose(&mut rng);
     match pick {
         Some(p) => {
-            Ok(Card {
+            Some(Card {
                 name: p.to_string(),
                 text: p.replace(".jpg", "-text.png"),
             })
         }
-        None => Err("Nothing Picked!".to_string()),
+        None => None
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,12 @@ const CARD_DIRECTORY: &str = "cards";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let cards = get_cards(CARD_DIRECTORY)?;
-    if cards.is_empty() {
-        panic!("No cards loaded from {}", CARD_DIRECTORY);
-    };
+    println!("Caching card list!");
+    let cards = get_cards(CARD_DIRECTORY)
+        .expect("No cards loaded from card directory");
     println!("Pulled {} cards from {}", cards.len(), CARD_DIRECTORY);
 
     let configuration = get_configuration().expect("failed to read config.");
-
     let listener = TcpListener::bind((
         configuration.application_address,
         configuration.application_port,
