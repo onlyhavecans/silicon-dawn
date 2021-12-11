2c3a65192f0b292480aec049f6689888152e8bf7
Amelia Aronsohn <squirrel@wearing.black>
2021-12-11 07:41:04 -0800

    Refactor my card filter out

--- src/cards.rs
+++ src/cards.rs
@@ -2,6 +2,7 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::ffi::OsStr;
 use std::fs;
+use std::fs::ReadDir;
 use std::path::Path;
 
 pub type CardDeck = Vec<String>;
@@ -14,15 +15,8 @@ pub struct Card {
 pub fn get_cards(directory: &str) -> Option<CardDeck> {
     let dir = Path::new(directory);
     let files = fs::read_dir(dir).ok()?;
-    let extension: &OsStr = OsStr::new("jpg");
 
-    let cards: CardDeck = files
-        .filter(|x| x.is_ok())
-        .map(|x| x.unwrap())
-        .filter(|x| x.path().extension() == Some(extension))
-        .map(|x| x.file_name().into_string().unwrap())
-        .collect();
-
+    let cards = get_cards_from_dir(files);
     match cards.is_empty() {
         true => None,
         false => Some(cards),
@@ -36,4 +30,16 @@ pub fn pick_a_card(cards: &[String]) -> Option<Card> {
         name: p.to_string(),
         text: p.replace(".jpg", "-text.png"),
     })
+}
+
+fn get_cards_from_dir(files: ReadDir) -> CardDeck {
+    let extension: &OsStr = OsStr::new("jpg");
+
+    let cards: CardDeck = files
+        .filter(|x| x.is_ok())
+        .map(|x| x.unwrap())
+        .filter(|x| x.path().extension() == Some(extension))
+        .map(|x| x.file_name().into_string().unwrap())
+        .collect();
+    cards
 }
